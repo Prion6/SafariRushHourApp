eb1e8f9f765d8eb789fdb3d7ebad821ad9c6c025
Prion6 <gsepulveda17@alumnos.utalca.cl>
2021-01-06 20:12:37 -0300

    Create PHP Queries
    PHPQuerySet contains the queries to the SQL Database as a Scriptable object
    
    Instance of PHPQuerySet created into resources

---
+++ SafariRushApp/Assets/Networking/UnityScripts/PHPQuerySet.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[CreateAssetMenu(menuName = "Manager/PHPQuerySet")]
+public class PHPQuerySet : ScriptableObject
+{
+    public string URL;
+
+    public IEnumerator Testing(string URN)
+    {
+        UnityWebRequest www = UnityWebRequest.Get(URL + URN + ".php");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+    }
+
+    public IEnumerator GetPuzzle(string URN, DIFFICULTY difficulty, System.Action<string> callback)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("difficulty", (int)difficulty);
+        
+        using (UnityWebRequest www = UnityWebRequest.Post(URL + URN + ".php", form))
+        {
+            www.timeout = 1;
+
+            yield return www.SendWebRequest();
+            
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                callback(www.downloadHandler.text);
+                //Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+}
