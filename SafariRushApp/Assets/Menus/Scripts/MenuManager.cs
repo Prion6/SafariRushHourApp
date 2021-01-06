092387d25c6229763eee5b9050ce51a6930eff41
Prion6 <gsepulveda17@alumnos.utalca.cl>
2021-01-06 20:11:04 -0300

    Create Menu Manager and Main Menu Manager
    Menu Manager is parent class for al menu managers
    
    Main menu manager extends from menu manager

---
+++ SafariRushApp/Assets/Menus/Scripts/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public virtual void LoadScene(int sceneID)
+    {
+        GameManager.LoadScene(sceneID);
+    }
+
+    public virtual void LoadScene(string sceneName)
+    {
+        GameManager.LoadScene(sceneName);
+    }
+}
