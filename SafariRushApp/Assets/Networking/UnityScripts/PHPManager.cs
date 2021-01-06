602816a4696dcf2162590fe16fc10e727cd0a09f
Prion6 <gsepulveda17@alumnos.utalca.cl>
2021-01-06 20:13:11 -0300

    Create PHP Manager
    PHP Manager calls queries from PHPQuerySet

---
+++ SafariRushApp/Assets/Networking/UnityScripts/PHPManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PHPManager : MonoBehaviour
+{
+    PHPQuerySet querier;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        querier = Resources.Load("PHP Query Set") as PHPQuerySet;
+    }
+
+    public void Test()
+    {
+        StartCoroutine(querier.Testing("Test"));
+    }
+
+    public void SetPuzzleScene(DIFFICULTY difficulty)
+    {
+        StartCoroutine(GetPuzzle(difficulty));
+    }
+    
+    public IEnumerator GetPuzzle(DIFFICULTY difficulty)
+    {
+        yield return StartCoroutine(querier.GetPuzzle("GetPuzzle", difficulty, (s) => GameManager.Puzzle = s));
+        Debug.Log(GameManager.Puzzle);
+        GameManager.LoadScene("Puzzle");
+    }
+}
