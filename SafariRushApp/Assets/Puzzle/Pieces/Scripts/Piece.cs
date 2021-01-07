0e392da1aa49dfd39513b3eac986e7b4a5151f29
Prion6 <gsepulveda17@alumnos.utalca.cl>
2021-01-07 15:22:45 -0300

    Piece Script

---
+++ SafariRushApp/Assets/Puzzle/Pieces/Scripts/Piece.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public Vector2 dimension;
+    public char identifier;
+    public List<Vector2> coordenates;
+    public PieceType type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
+
+public enum PieceType
+{
+    ROVER,
+    TWO_BY_TWO,
+    TWO_BY_ONE,
+    THREE_BY_ONE,
+    WALL,
+    EMPTY
+}
