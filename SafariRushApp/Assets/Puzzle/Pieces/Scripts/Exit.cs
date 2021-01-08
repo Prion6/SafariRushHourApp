f5e5195b1c04149f8361e2a79b32a9fab237518c
Prion6 <gsepulveda17@alumnos.utalca.cl>
2021-01-08 15:26:01 -0300

    Win Condition

---
+++ SafariRushApp/Assets/Puzzle/Pieces/Scripts/Exit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : Piece
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Piece p = other.gameObject.GetComponent<Piece>();
+        if (p == null) return;
+        if (p.type.Equals(PieceType.ROVER))
+        {
+            FindObjectOfType<PuzzleManager>().GameOver();
+        }
+    }
+}
