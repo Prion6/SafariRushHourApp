7829b3da7d66bbb443a46137f470e42d408ac974
Prion6 <gsepulveda17@alumnos.utalca.cl>
2021-01-13 17:16:09 -0300

    MultiLanguage System
    System to store the texts to use for each language to sue

---
+++ SafariRushApp/Assets/GameData/Languages/TextDataBase.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "DataBase/TextDatabase")]
+public class TextDataBase : ScriptableObject
+{
+    public Langauge langauge;
+    public List<UIText> texts;
+    private Dictionary<string, string> _Dictionary;
+    public Dictionary<string, string> Dictionary
+    { get
+        {
+            if(_Dictionary == null)
+            {
+                Load();
+            }
+            return _Dictionary;
+        }
+    }
+
+    public void Load()
+    {
+        _Dictionary = new Dictionary<string, string>();
+        foreach(UIText t in texts)
+        {
+            _Dictionary.Add(t.uiElement, t.text);
+        }
+    }
+}
+
+[System.Serializable]
+public struct UIText
+{
+    public string uiElement;
+    [TextArea]
+    public string text;
+}
+
+public enum Langauge
+{
+    ENGLISH,
+    SPANISH
+}
