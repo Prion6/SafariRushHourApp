dca5a21aef1ccdd7124586d1859883f0885eb0b1
Prion6 <gsepulveda17@alumnos.utalca.cl>
2021-01-15 17:42:14 -0300

    Move and Time counter

---
+++ SafariRushApp/Assets/Puzzle/Scripts/UIController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIController : MonoBehaviour
+{
+    public Text timeTxt;
+    public Text timeCounterTxt;
+    public Text movesTxt;
+    public Text movesCounterTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetTime(double seconds)
+    {
+        timeCounterTxt.text = ((int)seconds).ToString();
+    }
+
+    public void SetMoves(int moves)
+    {
+        movesCounterTxt.text = moves.ToString();
+    }
+}
