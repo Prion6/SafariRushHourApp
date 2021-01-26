e5dce46d6108569adf859a546fd401a6c2d56ff8
Prion6 <gsepulveda17@alumnos.utalca.cl>
2021-01-26 17:37:12 -0300

    Slab code
    Slab code to change materials

---
+++ SafariRushApp/Assets/Puzzle/Pieces/Scripts/Slab.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slab : MonoBehaviour
+{
+    public Material selected;
+    public Material hint;
+    public Material normal;
+    public Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SetSelected(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetSelected(bool b)
+    {
+        if(b)
+        {
+            rend.material = selected;
+        }
+        else
+        {
+            rend.material = normal;
+        }
+    }
+
+    public void SetHint(bool b)
+    {
+        if (b)
+        {
+            rend.material = hint;
+        }
+        else
+        {
+            rend.material = normal;
+        }
+    }
+}
